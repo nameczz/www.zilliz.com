881d14a02619fdb61f0bbc1ef7bf2eeba90a8654
zhen.an <zhen.an@zilliz.com>
2019-06-25 19:23:17 +0800

    [stroy] add dropbox

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,24 +1,40 @@
 import React from "react"
 import { Link } from 'gatsby'
 import logo from "../images/logo.svg" // Tell Webpack this JS file uses this image
+import './Header.scss'
 
 const Header = () => {
+  let timeOut = undefined;
+  const handleDropDown = event =>{
+    const content = event.currentTarget.lastChild;
+    event.type === 'mouseenter'
+      ? content.className = "list-show"
+      : timeOut = setTimeout(() => { content.className = "list-hide" },100) ;
+      return false;
+  }
+  const handleSublist = event => {
+    if(timeOut) clearTimeout(timeOut);
+    const {currentTarget, type} = event;
+    type === 'mouseenter'
+      ? currentTarget.className = 'list-show'
+      : currentTarget.className = 'list-hide'
+  }
   return (
     <header className="bg-dark">
       <div className="container">
         <nav className="navbar navbar-expand-lg">
-          <a className="navbar-brand" href="https://www.zilliz.com">
-            <img src={logo} height="30" alt="logo" />
+          <a className="navbar-brand" href="https://www.zilliz.com" style={{ padding: '0 1rem', overflow:'visible'}}>
+            <img src={logo} height="40" alt="logo" />
           </a>
-
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav13" aria-controls="navbarNav13" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
           <div className="collapse navbar-collapse" id="navbarNav13">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="https://www.zilliz.com">Home <span className="sr-only">(current)</span></a>
+              <li className='drop-down'  onMouseEnter={ handleDropDown } onMouseLeave={handleDropDown}>
+                <a className="nav-link" href="">Products <span className="sr-only">(current)</span></a>
+                <ul className='list-hide' onMouseEnter={ handleSublist } onMouseLeave={handleSublist}>
+                  <li><Link to="">asdasd</Link></li>
+                  <li><Link to="">asdasd</Link></li>
+                  <li><Link to="">asdasd</Link></li>
+                </ul>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/why-zilliz">Why Zilliz</Link>
