3908bec323a863fa5596418e3f1c458d3ba4e443
zhen.an <zhen.an@zilliz.com>
2019-06-26 09:19:06 +0800

    [Story] add about-us and news

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -5,55 +5,70 @@ import './Header.scss'
 
 const Header = () => {
   let timeOut = undefined;
-  const handleDropDown = event =>{
+  const handleDropDown = event => {
     const content = event.currentTarget.lastChild;
     event.type === 'mouseenter'
-      ? content.className = "list-show"
-      : timeOut = setTimeout(() => { content.className = "list-hide" },100) ;
-      return false;
+      ? content.classList.add('list-show')
+      : timeOut = setTimeout(() => { content.classList.remove('list-show') }, 100);
+    return false;
   }
   const handleSublist = event => {
-    if(timeOut) clearTimeout(timeOut);
-    const {currentTarget, type} = event;
+    if (timeOut) clearTimeout(timeOut);
+    const { currentTarget, type } = event;
     type === 'mouseenter'
-      ? currentTarget.className = 'list-show'
-      : currentTarget.className = 'list-hide'
+      ? console.log('')
+      : currentTarget.classList.remove('list-hide')
   }
   return (
     <header className="bg-dark">
       <div className="container">
-        <nav className="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg" style={{padding: 0}}>
           <Link to="/" className="navbar-brand">
-            <img src={logo} height="30" alt="logo" />
+            <img src={logo} height="50" alt="logo" style={{overflow: 'visible '}} />
           </Link>
-
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav13" aria-controls="navbarNav13" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
           <div className="collapse navbar-collapse" id="navbarNav13">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/why-zilliz">Why Zilliz</Link>
               </li>
-              <li className="nav-item">
+              <li className='nav-item drop-down'  onMouseEnter={handleDropDown} onMouseLeave={handleDropDown}>
+                <a className='nav-link' href="javascript:return false;">Product
+                    <svg t="1561497657372" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3057" width="16" height="16"><path d="M517.61 688.417c9.004 0.189 18.004-3.11 24.837-9.945l279.402-279.402c13.197-13.197 13.243-34.926-0.141-48.311-13.479-13.479-35.02-13.434-48.311-0.141l-255.741 255.741-255.741-255.741c-13.293-13.293-34.832-13.34-48.311 0.141-13.385 13.385-13.34 35.113-0.141 48.311l279.402 279.402c6.833 6.833 15.838 10.181 24.837 9.945z" p-id="3058" fill="#ffffff"></path></svg>
+                    {/* <svg t="1561497913193" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3417" width="16" height="16"><path d="M506.39 335.583c-9.004-0.189-18.004 3.11-24.837 9.945l-279.402 279.402c-13.197 13.197-13.243 34.926 0.141 48.311 13.479 13.479 35.020 13.434 48.311 0.141l255.741-255.741 255.741 255.741c13.293 13.293 34.832 13.34 48.311-0.141 13.385-13.385 13.34-35.113 0.141-48.311l-279.402-279.402c-6.833-6.833-15.838-10.181-24.837-9.945z" p-id="3418" fill="#ffffff"></path></svg> */}
+                </a>
+                <ul className='list' onMouseEnter={handleSublist} onMouseLeave={handleSublist}>
+                  <li className='product'><Link className='nav-link' to='/milvus'>Milvus</Link></li>
+                  <li className='product'><Link className='nav-link' to='/MegaWise'>MegaWise</Link></li>
+                </ul>
+              </li>
+              {/* <li className="nav-item">
                 <a className="nav-link" href="/milvus">Milvus</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/megawise">MegaWise</a>
-              </li>
+              </li> */}
               <li className="nav-item">
                 <Link className="nav-link" to="/news">News</Link>
               </li>
             </ul>
 
             <ul className="navbar-nav justify-content-end ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.zilliz.com">Join us</a>
+              {/* <li className="nav-item">
+                <a className="nav-link" href="https://www.zilliz.com">About us</a>
+              </li> */}
+              <li className='nav-item drop-down'  onMouseEnter={handleDropDown} onMouseLeave={handleDropDown}>
+                <a className='nav-link' href="javascript:return false;">Our Culture
+                    <svg t="1561497657372" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3057" width="16" height="16"><path d="M517.61 688.417c9.004 0.189 18.004-3.11 24.837-9.945l279.402-279.402c13.197-13.197 13.243-34.926-0.141-48.311-13.479-13.479-35.02-13.434-48.311-0.141l-255.741 255.741-255.741-255.741c-13.293-13.293-34.832-13.34-48.311 0.141-13.385 13.385-13.34 35.113-0.141 48.311l279.402 279.402c6.833 6.833 15.838 10.181 24.837 9.945z" p-id="3058" fill="#ffffff"></path></svg>
+                    {/* <svg t="1561497913193" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3417" width="16" height="16"><path d="M506.39 335.583c-9.004-0.189-18.004 3.11-24.837 9.945l-279.402 279.402c-13.197 13.197-13.243 34.926 0.141 48.311 13.479 13.479 35.020 13.434 48.311 0.141l255.741-255.741 255.741 255.741c13.293 13.293 34.832 13.34 48.311-0.141 13.385-13.385 13.34-35.113 0.141-48.311l-279.402-279.402c-6.833-6.833-15.838-10.181-24.837-9.945z" p-id="3418" fill="#ffffff"></path></svg> */}
+                </a>
+                <ul className='list' onMouseEnter={handleSublist} onMouseLeave={handleSublist}>
+                  <li className='about-us'><Link className='nav-link' to='/about-us'>About us</Link></li>
+                  <li className='about-us'><Link className='nav-link' to='/joinus'>Join us</Link></li>
+                </ul>
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <a className="nav-link" href="https://www.zilliz.com">Contact</a>
-              </li>
+              </li> */}
             </ul>
 
             {/* <a className="btn btn-light ml-md-3" href="https://www.zilliz.com">Button</a> */}
---
+++ src/pages/news.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from "../components/layout";
+import './about-us.scss'
+import CEO from '../images/photos/ceo.png'
+
+const WhyZilliz = () => (
+  <Layout>
+    <section className="fdb-block" data-block-type="contents" data-id="4" draggable="true">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-10 col-sm-6 col-md-5 col-lg-4 m-auto pb-5 pb-md-0" >
+            <img alt="image" className="img-fluid rounded-0" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//draws/iphone-hand.svg" />
+          </div>
+          <div className="col-12 ml-md-auto col-md-7 col-lg-6 pb-5 pb-md-0">
+            <h1>Zilliz简介</h1>
+            <p>ZILLIZ成立于2016年，专注于研发基于异构众核加速的新一代数据库系统。</p>
+            <p>公司核心产品MegaWise数据库和传统数据库相比，查询性能提升100倍以上，硬件和运维成本降低10倍。同时，ZILLIZ提供使用GPU从加速数据ETL，到加速数据仓库，再到加速AI模型训练，最后到加速数据可视化的端到端数据智能整体解决方案。ZILLIZ的产品和解决方案在金融、电信、医疗、智慧城市和电子商务等领域有着广泛应用前景。</p>
+            <p>
+              目前，ZILLIZ已经获得来自多家一线投资机构的上亿元人民币投资。</p>
+            <p className="mt-4">
+              <Link className="btn btn-primary" to='/why-zilliz'>Why Zilliz</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="fdb-block" data-block-type="contents" data-id="4" draggable="true">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-10 col-sm-6 col-md-5 col-lg-4 m-auto pb-5 pb-md-0" >
+            <h1>StarLoad</h1>
+            <p className="lead">Founder && CEO </p>
+            <ul className='list' >
+              <li>数据库专家，深耕数据库行业16年</li>
+              <li>在Oracle美国总部工作多年，负责数据库系统核心研发</li>
+              <li>作为奠基人研发的Oracle 12c 多租户数据库产品已累计产生超过20亿美金的销售收入</li>
+            </ul>
+            <p className="mt-4 mb-5"><a className="btn btn-secondary" href="https://www.froala.com">Learn More</a></p>
+
+          </div>
+          <div className="col-12 ml-md-auto col-md-7 col-lg-6 pb-5 pb-md-0">
+            <img alt="image" className="img-fluid rounded-0" src={CEO} />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="fdb-block team-2 fp-active" data-block-type="teams" data-id="3" draggable="true">
+      <div className="container">
+        <div className="row text-center justify-content-center">
+          <div className="col-8">
+            <h1>Meet Our Team</h1>
+          </div>
+        </div>
+
+        <div className="row-50"></div>
+
+        <div className="row text-center justify-content-center">
+          <div className="col-sm-3 m-sm-auto">
+            <img alt="image" className="img-fluid rounded-circle" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//people/4.jpg" />
+
+            <h2>Sara Doe</h2>
+            <p>Founder</p>
+          </div>
+
+          <div className="col-sm-3 m-sm-auto">
+            <img alt="image" className="img-fluid rounded-circle" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//people/5.jpg" />
+
+            <h2>Sara Doe</h2>
+            <p>Founder</p>
+          </div>
+
+          <div className="col-sm-3 m-sm-auto">
+            <img alt="image" className="img-fluid rounded-circle" src="https://cdn.jsdelivr.net/gh/froala/design-blocks@2.0.1/dist/imgs//people/7.jpg" />
+
+            <h2>Sara Doe</h2>
+
+            <p>Founder</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default WhyZilliz
