75708a13fb1cc14a2fb12f4214ca7f1029d4535a
zhen.an <zhen.an@zilliz.com>
2019-06-27 15:22:00 +0800

    try blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,75 @@
-import React from 'react'
-import Layout from "../components/layout";
-import SEO from '../components/seo';
+// import React from 'react';
+// import { graphql } from 'gatsby';
+// import Layout from "../components/layout";
+// import SEO from '../components/seo';
 
-const Blog = props => {
+// export default ({ data }) => {
+//   console.log(data);
+//   const { markdownRemark } = data // data.markdownRemark holds our post data
+//   const { frontmatter, html } = markdownRemark
+//   return (
+//     <Layout>
+//       <SEO title='blog' />
+//       <div>this is blog page</div>
+//       <div className="blog-post-container">
+//         <div className="blog-post">
+//           <h1>{frontmatter.title}</h1>
+//           <h2>{frontmatter.date}</h2>
+//           <div
+//             className="blog-post-content"
+//             dangerouslySetInnerHTML={{ __html: html }}
+//           />
+//         </div>
+//       </div>
+//     </Layout>
+//   )
+// }
+
+// export const pageQuery = graphql`
+//   query($path: String!) {
+//     markdownRemark(frontmatter: { path: { eq: $path } }) {
+//       html
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         path
+//         title
+//       }
+//     }
+//   }
+// `
+
+
+import React from "react"
+import { graphql } from "gatsby"
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
   return (
-    <Layout>
-      <SEO title='blog' />
-      <div>this is blog page</div>
-    </Layout>
+    <div className="blog-post-container">
+      <div className="blog-post">
+        {/* <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        /> */}
+      </div>
+    </div>
   )
 }
 
-export default Blog
+// export const pageQuery = graphql`
+//   query($path: String!) {
+//     markdownRemark(frontmatter: { path: { eq: $path } }) {
+//       html
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         path
+//         title
+//       }
+//     }
+//   }
+// `
