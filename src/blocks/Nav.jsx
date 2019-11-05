5ccf1a8069f4b882557ac51ac79c2a6d82c5acff
nameczz <nameczz@nameczzdeMacBook-Pro.local>
2019-11-05 14:04:41 +0800

    add mobile ui

--- src/blocks/Nav.jsx
+++ src/blocks/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LocalizeLink from "../components/localizedLink";
 import logo from "../images/logo.svg"; // Tell Webpack this JS file uses this image
 import "./Nav.scss";
@@ -44,56 +44,96 @@ const Current = () => (
     />
   </svg>
 );
-const Nav = ({ data, locale, nav = {}, subNav = <></> }) => {
+
+const PcNav = ({ data, locale, nav = {}, subNav = <></> }) => {
   return (
     <>
       <nav className="wrapper nav-wrapper">
         <div className="inner-container nav">
+          <div className="left">
+            <LocalizeLink locale={locale} to="/" className="logo">
+              <img src={logo} alt="logo" />
+            </LocalizeLink>
+            <ul className="nav-links">
+              <li>
+                {nav.current === "index" && <Current />}
+                <LocalizeLink
+                  className={nav.current === "index" ? "current" : ""}
+                  locale={locale}
+                  to="/"
+                >
+                  {data.product}
+                </LocalizeLink>
+              </li>
+              <li>
+                {nav.current === "aboutus" && <Current />}
+                <LocalizeLink
+                  className={nav.current === "aboutus" ? "current" : ""}
+                  locale={locale}
+                  to="/aboutus"
+                  target="_blank"
+                >
+                  {data.aboutus}
+                </LocalizeLink>
+              </li>
+              <li>
+                {nav.current === "joinUs" && <Current />}
+                <a href="https://zilliz.gllue.com/portal/zilliz">
+                  {data.joinus}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <a
+            className="right"
+            href="http://infini-analytics.github.io/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {data.doc}
+          </a>
+        </div>
+      </nav>
+      {subNav}
+    </>
+  );
+};
+
+const MobileNav = ({ data, locale, nav = {} }) => {
+  return (
+    <nav className="wrapper nav-wrapper">
+      <div className="inner-container nav">
         <div className="left">
           <LocalizeLink locale={locale} to="/" className="logo">
             <img src={logo} alt="logo" />
           </LocalizeLink>
-          <ul className="nav-links">
-            <li>
-              {nav.current === "index" && <Current />}
-              <LocalizeLink
-                className={nav.current === "index" ? "current" : ""}
-                locale={locale}
-                to="/"
-              >
-                {data.product}
-              </LocalizeLink>
-            </li>
-            <li>
-              {nav.current === "aboutus" && <Current />}
-              <LocalizeLink
-                className={nav.current === "aboutus" ? "current" : ""}
-                locale={locale}
-                to="/aboutus"
-                target="_blank"
-              >
-                {data.aboutus}
-              </LocalizeLink>
-            </li>
-            <li>
-              {nav.current === "joinUs" && <Current />}
-              <a href="https://zilliz.gllue.com/portal/zilliz">
-                {data.joinus}
-              </a>
-            </li>
-          </ul>
         </div>
-        <a
-          className="right"
-          href="http://infini-analytics.github.io/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {data.doc}
-        </a>
-        </div>
-      </nav>
-      {subNav}
+        <div className="right">liebiao</div>
+      </div>
+    </nav>
+  );
+};
+
+const Nav = ({ data, locale, nav = {}, subNav = <></> }) => {
+  const [screenWidth, setScreenWidth] = useState(null);
+  useEffect(() => {
+    setScreenWidth(document.body.clientWidth);
+    const cb = () => {
+      setScreenWidth(document.body.clientWidth);
+    };
+    window.addEventListener("resize", cb);
+    return () => {
+      window.removeEventListener("resize", cb);
+    };
+  }, []);
+
+  return (
+    <>
+      {screenWidth >= 1000 ? (
+        <PcNav data={data} locale={locale} nav={nav} subNav={subNav}></PcNav>
+      ) : (
+        <MobileNav data={data} locale={locale} nav={nav}></MobileNav>
+      )}
     </>
   );
 };
