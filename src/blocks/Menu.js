7b7bc4dd222e9b58ff795ce162c65454155c07d2
chenzizhao <zizhao.chen@zilliz.com>
2019-11-07 10:25:57 +0800

    add menu transition

--- src/blocks/Menu.js
+++ src/blocks/Menu.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import LocalizeLink from "../components/localizedLink";
-import './Menu.scss'
+import arrow from "../images/icons/arrow.svg";
+import "./Menu.scss";
 
 const findItem = (key, arr) => {
   let find = undefined;
   arr.forEach(v => {
     if (v.title === key) {
       find = v;
-      return
+      return;
     }
     if (v.children && v.children.length) {
       find = findItem(key, v.children);
@@ -28,9 +29,9 @@ const Menu = props => {
         .sort((a, b) => a[a.length - 1] - b[b.length - 1]);
       let index = 0;
       return function innerFn(formatMenu = []) {
-        let copyMenu = JSON.parse(JSON.stringify(formatMenu))
+        let copyMenu = JSON.parse(JSON.stringify(formatMenu));
         const parentLabel = index ? labelKeys[index - 1] : "";
-        
+
         if (index && !parentLabel) {
           return copyMenu;
         }
@@ -47,12 +48,12 @@ const Menu = props => {
           const item = {
             ...v,
             children: [],
+            showChildren: false,
           };
           if (index === 0) {
             copyMenu.push(item);
           } else {
             const parent = findItem(v[parentLabel], copyMenu);
-
             parent && parent.children.push(item);
           }
         });
@@ -61,28 +62,48 @@ const Menu = props => {
         return innerFn(copyMenu);
       };
     };
-    
 
     setRealMenuList(generateMenu(menuList)());
   }, [menuList]);
 
-  const generageMenuDom = (list, className="") =>{
-    return list.map(doc => (
+  const generageMenuDom = (list, className = "") => {
+    return list.map((doc, index) => (
       <div className={className} key={doc.id}>
-        <LocalizeLink locale={doc.lang} to={`${preLink}/${doc.id}`}>
-          {doc.title}
-        </LocalizeLink>
-        {
-          doc.children && doc.children.length ?
-            generageMenuDom(doc.children, 'menu-child') : null
-        }
+        <div className="menu_name-wrapper">
+          <LocalizeLink locale={doc.lang} to={`${preLink}/${doc.id}`}>
+            {doc.title}
+          </LocalizeLink>
+
+          {doc.children && doc.children.length ? (
+            <img
+              src={arrow}
+              alt="arrow"
+              className={`arrow ${doc.showChildren ? 'top' : ''}`}
+              onClick={() => {
+                toggleMenu(doc);
+              }}
+            ></img>
+          ) : null}
+        </div>
+        <div className={`menu-child-wrapper ${doc.showChildren ? 'open' : ''}`} >
+          {doc.children && doc.children.length
+            ? generageMenuDom(doc.children, "menu-child")
+            : null}
+        </div>
       </div>
-    ))
-  }
+    ));
+  };
+
+  const toggleMenu = doc => {
+    const copyMenu = JSON.parse(JSON.stringify(realMenuList))
+    const findDoc = findItem(doc.title, copyMenu);
+    findDoc.showChildren = !findDoc.showChildren
+    setRealMenuList(copyMenu);
+  };
 
   return (
     <section className="menu-container">
-      {generageMenuDom(realMenuList, 'menu-item')}
+      {generageMenuDom(realMenuList, "menu-top-level")}
     </section>
   );
 };
