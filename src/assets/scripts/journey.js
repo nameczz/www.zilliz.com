18258423c82a588964890dfe008ad16d1f2e3a96
chenzizhao <nameczz@126.com>
2020-04-13 18:17:46 +0800

    add header mobile

--- src/assets/scripts/journey.js
+++ src/assets/scripts/journey.js
@@ -1,9 +1,14 @@
-const windowWidth = $(document).width();
-console.log(windowWidth);
-if (windowWidth <= 1024) {
-  $("#journey-pc").hide();
-  $("#journey-mobile").show();
-} else {
-  $("#journey-pc").show();
-  $("#journey-mobile").hide();
-}
+const cb = () => {
+  const windowWidth = $(document).width();
+  console.log(windowWidth);
+
+  if (windowWidth <= 1024) {
+    $("#journey-pc").hide();
+    $("#journey-mobile").show();
+  } else {
+    $("#journey-pc").show();
+    $("#journey-mobile").hide();
+  }
+};
+window.addEventListener("resize", cb);
+cb();
--- src/assets/scripts/layout.js
+++ src/assets/scripts/layout.js
@@ -4,3 +4,12 @@ if (window.location.pathname === "/") {
   $("#layout-header").css("background-color", "transparent");
   $("#layout-header").css("box-shadow", "none");
 }
+
+$("#mobile-list").on("click", () => {
+  $("#header-route").animate(
+    {
+      height: "toggle",
+    },
+    300
+  );
+});
