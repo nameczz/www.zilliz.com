245ef77e000d94db86476ae31d453905caa4cb0c
chenzizhao <nameczz@126.com>
2020-04-13 16:47:18 +0800

    improve ui

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,36 +1,41 @@
-const video = $("#home-video")[0]
+const video = $("#home-video")[0];
+let loaded = false;
 // beacuse every page will excute this script
 if (video) {
-  const totalTime = video.duration
-  const videoHeight = video.videoHeight
-  const videoOffsetTop = $(video).offset().top
-  const contentHeight = Math.round(totalTime * (videoHeight + videoOffsetTop))
-  const windowHeight = $(window).height()
-  let scrollPosition = $(document).scrollTop()
-  $("#video-wrapper").height(contentHeight)
-  console.log(totalTime)
+  video.addEventListener("canplay", function () {
+    console.log("canplay");
+    // only excute once
+    if (loaded) {
+      return;
+    }
+    loaded = true;
+    const totalTime = video.duration;
+    const videoHeight = video.videoHeight;
+    const videoOffsetTop = $(video).offset().top;
+    const contentHeight = Math.round(
+      totalTime * (videoHeight + videoOffsetTop)
+    );
+    const windowHeight = $(window).height();
 
-  window.addEventListener("scroll", (e) => {
-    if (!$(video)) {
-      return
-    }
-    const scrollHeight = $(document).scrollTop()
-    const direction = scrollHeight > scrollPosition ? "down" : "up"
-    const step = scrollHeight / contentHeight
-    console.log(video.currentTime, scrollHeight, contentHeight)
-    direction === "down" ? video.currentTime = step * totalTime : video.currentTime = step * totalTime
-    if (scrollHeight >= contentHeight - windowHeight) {
-      $(video).toggleClass("fixed-video", false)
-      $(video).toggleClass("bottom-video", true)
-    } else if (scrollHeight >= videoOffsetTop) {
-      console.log("in")
-      $(video).toggleClass("fixed-video", true)
-      $(video).toggleClass("bottom-video", false)
-    } else {
-      $(video).toggleClass("fixed-video", false)
-      $(video).toggleClass("bottom-video", false)
-    }
-    scrollPosition = scrollHeight
-  })
+    $("#video-wrapper").height(contentHeight);
+    window.addEventListener("scroll", (e) => {
+      if (!$(video)) {
+        return;
+      }
+      const scrollHeight = $(document).scrollTop();
+      const step = scrollHeight / contentHeight;
+      video.currentTime = step * totalTime;
+      if (scrollHeight >= contentHeight - windowHeight) {
+        $("#video-title-wrapper").toggleClass("fixed-video", false);
+        $("#video-title-wrapper").toggleClass("bottom-video", true);
+      } else if (scrollHeight >= videoOffsetTop) {
+        console.log("in");
+        $("#video-title-wrapper").toggleClass("fixed-video", true);
+        $("#video-title-wrapper").toggleClass("bottom-video", false);
+      } else {
+        $("#video-title-wrapper").toggleClass("fixed-video", false);
+        $("#video-title-wrapper").toggleClass("bottom-video", false);
+      }
+    });
+  });
 }
-
